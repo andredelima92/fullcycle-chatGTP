5c5b7020a3ff5d2afe9d7492f6558848d81ec7ec
Andre <andredelima92@gmail.com>
2023-04-17 10:54:15 -0300

    Added ChatConfig params

--- chatservice/internal/domain/entities/chat.go
+++ chatservice/internal/domain/entities/chat.go
@@ -1,7 +1,14 @@
 package entities
 
 type ChatConfig struct {
-	Model *Model
+	Model            *Model
+	Temperature      float32
+	TopP             float32
+	N                int
+	Stop             []string
+	MaxTokens        int
+	PresencePenalty  float32
+	FrequencyPenalty float32
 }
 
 type Chat struct {
